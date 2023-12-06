f7114735495e54ec623c9060b915a510fb7ba4cd
Lidor Blustein <liidorr@gmail.com>
2023-12-06 21:53:00 +0200

    js and html updates

--- index.js
+++ index.js
@@ -1,30 +1,27 @@
-// List of workout exercises
-const exercises = [
-    "Push-ups",
-    "Squats",
-    "Jumping Jacks",
-    "Plank",
-    "Lunges",
-    "Burpees",
-    "Mountain Climbers",
-    "High Knees",
-    "Sit-ups",
-    "Dumbbell Rows"
-];
+// Object with exercises categorized by muscle groups
+const exercisesByMuscle = {
+    legs: ["Squats", "Lunges", "Jumping Jacks", ""],
+    back: ["Dumbbell Rows", "Pull-ups", "Deadlifts"],
+    chest: ["Push-ups", "Bench Press", "Dumbbell Flyes"],
+    arms: ["Bicep Curls", "Tricep Dips", "Hammer Curls"],
+    abs: ["Plank", "Sit-ups", "Leg Raises"],
+    shoulders: ["Shoulder Press", "Lateral Raises", "Front Raises"]
+};
 
-// Function to generate a random exercise
-function generateRandomExercise() {
-    const randomIndex = Math.floor(Math.random() * exercises.length);
-    return exercises[randomIndex];
-}
-
-// Function to update the exercise display
-function updateExerciseDisplay() {
+// Function to generate a random exercise for each muscle group
+function generateRandomExercises() {
     const exerciseDisplay = document.getElementById("exerciseDisplay");
-    const exercise = generateRandomExercise();
-    exerciseDisplay.textContent = `Today's Exercise: ${exercise}`;
+
+    for (const muscle in exercisesByMuscle) {
+        if (exercisesByMuscle.hasOwnProperty(muscle)) {
+            const randomIndex = Math.floor(Math.random() * exercisesByMuscle[muscle].length);
+            const randomExercise = exercisesByMuscle[muscle][randomIndex];
+            const muscleElement = document.getElementById(muscle);
+            muscleElement.textContent = `${muscle.charAt(0).toUpperCase() + muscle.slice(1)}: ${randomExercise}`;
+        }
+    }
 }
 
 // Event listener for button click
 const generateExerciseBtn = document.getElementById("generateExerciseBtn");
-generateExerciseBtn.addEventListener("click", updateExerciseDisplay);
+generateExerciseBtn.addEventListener("click", generateRandomExercises);
